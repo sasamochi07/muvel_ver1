daf29398398cd688153b591ef331f4f484240d21
sasamochi07 <sasamotk@gmail.com>
2020-10-06 01:04:10 +0900

    commit

--- resources/views/discover.blade.php
+++ resources/views/discover.blade.php
@@ -1,21 +1,28 @@
+@extends('layouts.app')
+@section('content')
+@include('common.errors')
+
 <div class="row">
     <div class="col-sm-4">
                     
         <div class="text-center my-4">
             <h3 class="brown border p-2">音楽検索</h3>
         </div>
-        {!! Form::open(['route' => 'select', 'method' => 'get']) !!}
 
-            <div class="form-group">
+        <tbody>
+             {!! Form::open(['route' => 'select', 'method' => 'get']) !!}
+
+            <td class="form-group">
                 {!! Form::label('genre_id', 'ジャンル:') !!}
                 {!! Form::select('genre_id', ['' => '指定なし'] + Config::get('genre.janru') , '指定なし') !!}
                 {!! Form::label('category_id', 'ジャンル:') !!}
                 {!! Form::select('category_id', ['' => '指定なし'] + Config::get('category.kategori') , '指定なし') !!}
-            </div>
+            </td>
                                 {{-- //送信ボタンで上記の内容を送信する --}}
             {!! Form::submit('検索', ['class' => 'btn btn-primary btn-block']) !!}
                                 {{-- //ここでフォームの終了タグ --}}
-        {!! Form::close() !!}
+            {!! Form::close() !!}
+        <tbody>
     </div>
 
                 {{-- //検索結果の一覧を表示する --}}
@@ -24,35 +31,42 @@
             <h3 class="brown p-2">音楽一覧</h3>
         </div>
 
-        <div class="container">
-           
+        <tbody>  
             @if(!empty($data))
-               
-                    @foreach($data as $item)
-                            <div class="row py-2 border-bottom text-center">
-                                
-                                {{-- 検索結果に合致する音楽の表示 --}}
-                                <div class="col-sm-4">
-                                    {{ $item->music_name }}
-                                    <img src="{{ asset('image/' .$item->music_image_path) }}" alt="hoge.png"> 
-                                    <audio id="myTune{{$item}}" controls controlsList="nofullscreen nodownload noremoteplayback">
-                                        <source  src="{{ asset($item->music_path)}}">
-                                    </audio>
-                                    {{ $item->music_description }}
 
-                                    {{-- 各音楽から音楽再生ページに遷移させたいが、 $musicsが上手く渡せてない --}}
-                                    {{-- <form action="{{ url('/musicplay' .'/' .$musics->id) }}" method="GET"> {{ csrf_field() }}
-                                        <button type="submit" class="btn">
-                                            To Playing Page
-                                        </button>
-                                    </form>     --}}
-                                </div>
-                            </div>
-                    @endforeach
-                </div>
+                @foreach($data as $item)
+
+                    <tr>
+                        <td>
+                            {{ $item->music_name }}
+                        </td>
+                        <td>
+                            <img src="{{ asset('image/' .$item->music_image_path) }}" alt="hoge.png"> 
+                        </td>
+                        <td>
+                            <audio id="myTune{{$item}}" controls controlsList="nofullscreen nodownload noremoteplayback">
+                                <source  src="{{ asset($item->music_path)}}">
+                            </audio>
+                        </td>
+                        <td>
+                            {{ $item->music_description }}
+                        </td>
+                        {{-- 各音楽から音楽再生ページに遷移させたいが、 $musicsが上手く渡せてない --}}
+                        {{-- <td>
+                            <form action="{{ url('/musicplay' .'/' .$musics->id) }}" method="GET"> {{ csrf_field() }}
+                                <button type="submit" class="btn">
+                                    To Playing Page
+                                </button>
+                            </form>  
+                        </td> --}}
+                    </tr>
+
+                @endforeach
                 {{ $data->appends(request()->input())->render('pagination::bootstrap-4') }}
-            @endif
-        </div>
+           
+            @endif    
+        </tbody>   
     </div>
 </div>
+
 
